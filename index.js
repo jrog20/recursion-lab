7cfcc348d169ada311412d60c7a5bcf7f0e2afe2
jrog20 <jaimerogstad@gmail.com>
2021-10-08 12:57:08 -0400

    commit tree 37fca8b1da121fed6fb083f1b69f11e936bf832d

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-// Code your solution here!
+function printString(myString) {
+	if (myString.length > 1) {
+		let mySubString = myString.substring(1, myString.length);
+		printString(mySubString);
+	} else {
+		return true;
+	}
+}
 
+// function reverseString(myString) {
+//   if(myString.length < 2){
+//     return myString
+//   } else {
+//     console.log(myString)
+//     return reverseString(myString.substring(1)) + myString[0]   
+//   }
+// }
